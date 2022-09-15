bf8691a5039c98a597621bdf11b21f4ce660c20c
aloysius thomas <aloysius.thomas@nthindex.com>
2022-09-15 12:19:02 +0530

    email backend changed

--- project/project/settings.py
+++ project/project/settings.py
@@ -107,9 +107,11 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'email@gmail.com'
-EMAIL_HOST_PASSWORD = '9tes'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'email@gmail.com'
+# EMAIL_HOST_PASSWORD = '9tes'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
